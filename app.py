0e931f9eb92477696896fae04ba6af1fea9df180
Kevin  Meng <meng_fr595@163.com>
2023-11-19 23:36:33 +0800

    Update app.py
    add download by filename

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
 import os 
 import gradio as gr
-from huggingface_hub import snapshot_download
+import huggingface_hub as hh
+from huggingface_hub import hf_hub_download
 from bypy import ByPy
 
 
-def download(repo_id):
-    out = snapshot_download(repo_id=repo_id,cache_dir="/app/data/",force_download =True)
-    path_name = repo_id.replace("/","---")
-    # 移动文件
-    os.system(f"mkdir -p /app/downloads/{path_name}")
-    os.system(f"mv -rf {out}/* /app/downloads/{path_name}")
-    out_path = f"/app/downloads/{path_name}"
+def format_size(bytes, precision=2):
+    """
+    Convert a file size in bytes to a human-readable format like KB, MB, GB, etc.
+    Huggingface use 1000 not 1024
+    """
+    units = ["B", "KB", "MB", "GB", "TB", "PB"]
+    size = float(bytes)
+    index = 0
+
+    while size >= 1000 and index < len(units) - 1:
+        index += 1
+        size /= 1000
+
+    return f"{size:.{precision}f} {units[index]}"
+
+
+def list_repo_files_info(repo_id,token=None):
+    data_ls = [] 
+    for file in list(hh.list_files_info(repo_id)):
+        data_ls.append([file.path,format_size(file.size)])
+    files = [file[0] for file in data_ls]
+    filenames = gr.Dropdown(choices = files, label="选择文件",allow_custom_value=True,multiselect=True)
+    return data_ls, filenames
+
+
+def download_file(repo_id,filenames):
+    print(filenames)
+    repo_name = repo_id.replace("/","---")
+    
+    for filename in filenames:
+        print(filename)
+        out = hh.hf_hub_download(repo_id=repo_id,filename=filename,local_dir=f"/data/download/{repo_name}",local_dir_use_symlinks=False,force_download =True)
+    out_path = f"/data/download/{repo_name}"
     return out_path
+
 
 
 def upload_by_path(source_path,dest_path):
     bp = ByPy()
     out = bp.syncup(
       source_path,
-      path_name
+      dest_path
     )
+    # out = "test"
+    
     return out
 
 def upload_by_file(source_path):
@@ -28,6 +58,7 @@ def upload_by_file(source_path):
       source_path,
       source_path.split("/")[-1]
     )
+    # out = "test"
     return out
 
 
@@ -42,15 +73,25 @@ def app():
 
         gr.Markdown("# Huggingface Download 😀")
 
+
         with gr.Row():
             with gr.Column():
-                input = gr.Textbox(placeholder="输入 repo_id")
+                repo = gr.Textbox(placeholder="输入 repo_id")
+                list_bt = gr.Button("获取文件清单", variant="primary")
+                
+                filenames = gr.Dropdown(label="选择文件",allow_custom_value=False,multiselect=True)
                 submit_bt = gr.Button("下载", variant="primary")
+                gr.Markdown("下载路径：")
+                output = gr.Markdown(label="对应路径")
+                
             with gr.Column():
-                output = gr.Textbox(label="路径")
+                output_df = gr.Dataframe(headers=['文件名', '文件大小'])
+                        
+            list_bt.click(list_repo_files_info, inputs=repo, outputs=[output_df,filenames])
 
-            
-            submit_bt.click(download,input,outputs= output)
+            submit_bt.click(download_file, [repo, filenames], outputs=output)
+
+        gr.Markdown("---")
 
         with gr.Row():
             bp = ByPy()
@@ -60,11 +101,11 @@ def app():
         with gr.Row():
             with gr.Column():
                 submit_by = gr.Button("上传", variant="primary")
-                result = gr.Textbox(placeholder="结果路径")
+                result = gr.Markdown(label="结果路径")
 
             with gr.Column():
                 pass
-            submit_by.click(upload_by_path,output,outputs= result)
+            submit_by.click(upload_by_file,output,outputs= result)
 
     return demo
 
@@ -72,4 +113,6 @@ def app():
 if __name__ == "__main__":
     demo = app()
     demo.launch(share=True)
+
+
 
