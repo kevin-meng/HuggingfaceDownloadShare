1b9dae1389f92460618adabeb749e6a16c8d0e68
Kevin  Meng <meng_fr595@163.com>
2023-11-19 23:39:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+
 mkdir -p /etc/data
 cd /etc/data
 git clone https://github.com/kevin-meng/HuggingfaceDownloadShare.git
 cd HuggingfaceDownloadShare
+mkdir download
 
 docker build -t mengkevin/gradio_hg .
-docker run -it -v /etc/data:/app/data  mengkevin/gradio_hg
+docker run --rm -it -v .:/data mengkevin/gradio_hg
